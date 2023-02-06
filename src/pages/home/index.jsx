7a78c6f80a08b627e6d2f52359d4344de06cae56
Hakan <yaman.hakan@std.izu.edu.tr>
2023-02-06 19:19:08 +0300

    first day

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Home=()=>{
+    return <h1 className="text-red-600 font-dancing text-center text-3xl  " >Home page</h1>
+}
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#ffbe33",
+        secondary:"#222831"
+      },
+      fontFamily:{
+        dancing:['Dancing Script', "cursive"]
+      }
+    },
+  },
+  plugins: [],
+}
