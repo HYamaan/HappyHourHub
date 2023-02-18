9d76e45d9cc68f68edf91ae313a5344b390618a4
Hakan <yaman.hakan@std.izu.edu.tr>
2023-02-19 00:35:35 +0300

    unauthorized page entry blocked

--- src/pages/admin/profile.jsx
+++ src/pages/admin/profile.jsx
@@ -1,80 +1,33 @@
 import React, {useState} from "react";
+import {signOut} from "next-auth/react";
 
-
-import {useFormik} from "formik";
-import {profileSchema} from "../../schema/profileSchema";
 import Image from "next/image";
 import Order from "../../components/admin/Order";
 import Product from "../../components/admin/product";
 import Category from "../../components/admin/Category";
 import Footer from "../../components/admin/Footer";
+import axios from "axios";
+import {useRouter} from "next/router";
+import {toast} from "react-toastify";
 
 
 const Profile = () => {
     const[tabs,setTabs]=useState(0)
+    const router =useRouter();
+    const closeAdminAccount = async ()=>{
+        try {
+            if(confirm("Are you sure you want to close your Admin Account")){
+        const res= await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/admin`);
 
-    const onSubmit = async (values, actions) => {
-        await new Promise((resolve, _) => setTimeout(resolve, 2000));
-        actions.resetForm();
+        if(res.status===200){
+            await router.push("/admin");
+            toast.success("Admin Account Closed!")
+        }
+            }
+        }catch (err){
+            console.log(err);
+        }
     }
-
-    const formik = useFormik({
-        initialValues: {
-            fullName: "", phoneNumber: "", email: "", address: "", job: "", bio: "",
-        }, onSubmit, validationSchema: profileSchema,
-    });
-
-    const inputsInf = [{
-        id: 1,
-        name: "fullName",
-        type: "text",
-        placeholder: "Your Full Name",
-        value: formik.values.fullName,
-        errorMessage: formik.errors.fullName,
-        touched: formik.touched.fullName
-    }, {
-        id: 2,
-        name: "email",
-        type: "email",
-        placeholder: "Your Email",
-        value: formik.values.email,
-        errorMessage: formik.errors.email,
-        touched: formik.touched.email
-    }, {
-        id: 3,
-        name: "phoneNumber",
-        type: "number",
-        placeholder: "Your Phone Number",
-        value: formik.values.phoneNumber,
-        errorMessage: formik.errors.phoneNumber,
-        touched: formik.touched.phoneNumber
-    }, {
-        id: 4,
-        name: "address",
-        type: "text",
-        placeholder: "Your Address",
-        value: formik.values.address,
-        errorMessage: formik.errors.address,
-        touched: formik.touched.address
-    }, {
-        id: 5,
-        name: "job",
-        type: "text",
-        placeholder: "Your Job",
-        value: formik.values.job,
-        errorMessage: formik.errors.job,
-        touched: formik.touched.job
-    }, {
-        id: 6,
-        name: "bio",
-        type: "text",
-        placeholder: "Your Bio",
-        value: formik.values.bio,
-        errorMessage: formik.errors.bio,
-        touched: formik.touched.bio
-    },
-
-    ];
 
     return <React.Fragment>
         <div className="flex md:flex-row flex-col px-10 gap-x-3 min-h-[calc(100vh_-_433px)] mb-20">
@@ -85,6 +38,7 @@ const Profile = () => {
                         alt="client1.jpg"
                         width={100}
                         height={100}
+                        priority={true}
                         className="rounded-full"
                     />
                     <b className="text-2xl mt-1"> Admin</b>
@@ -118,9 +72,9 @@ const Profile = () => {
                         </li>
                         <li className={`border w-full p-3 hover:bg-primary
                             cursor-pointer hover:text-white transition-all ${tabs === 4 ?"bg-primary text-tertiary" : ""}`}
-                            onClick={()=>setTabs(4)}>
+                        onClick={closeAdminAccount}>
                             <i className="fa fa-sign-out"></i>
-                            <button className="ml-1">Exit</button>
+                            <button className="ml-1" type="button" onClick={()=>signOut()}>Exit</button>
                         </li>
                     </ul>
                 </div>
@@ -131,5 +85,20 @@ const Profile = () => {
             {tabs===3 && (<Footer/>)}
         </div>
     </React.Fragment>
+}
+
+export const getServerSideProps = (ctx) => {
+    const myCookie = ctx.req?.cookies || "";
+    if (myCookie.token !== process.env.ADMIN_TOKEN) {
+        return {
+            redirect: {
+                destination: "/admin",
+                permanent: false,
+            }
+        }
+    }
+    return {
+        props: {},
+    }
 }
 export default Profile;
