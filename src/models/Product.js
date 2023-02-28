8f0e955e36154921ef5b48e192ccdde5e5688911
Hakan <h.yaman@birerp.com>
2023-02-28 22:51:13 +0300

    New Product

---
+++ src/models/Product.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    image: {
+        type: String, required: true,
+    }, title: {
+        type: String, required: true, maxlength: 60,
+    }, desc: {
+        type: String, required: true, maxlength: 300,
+    }, category: {
+        type: String, required: true,
+    }, prices: {
+        type: [Number], required: true,
+    }, extraOptions: {
+        type: [{
+            text: {type: String}, price: {type: Number}
+        }], required: true,
+    },
+
+}, {timestamps: true});
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
