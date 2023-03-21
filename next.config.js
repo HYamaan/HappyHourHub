e7ac88cd58ec4dbe2e77ce80fcbf33d69e5a46da
hakanyaman <hakanyaman5249@gmail.com>
2023-03-21 21:13:42 +0300

    npm run build hattası

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
