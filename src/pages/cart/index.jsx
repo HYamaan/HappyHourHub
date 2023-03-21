60ef92e9280825e12db8c6e92cd3e0d9b13e3698
hakanyaman <hakanyaman5249@gmail.com>
2023-03-21 21:10:34 +0300

    ürün artırma azaltma ve bunları Redux ile gönderdik. Aynı zamanda kullanıcı siparişe bastığında aldığı ürünleride gönderdi.

--- src/pages/cart/index.jsx
+++ src/pages/cart/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Image from "next/image";
 import Title from "../../components/UI/Title";
 import {useSelector,useDispatch} from "react-redux";
@@ -8,15 +8,34 @@ import {useSession} from "next-auth/react";
 import {toast} from "react-toastify";
 import {useRouter} from "next/router";
 
-
 const Cart = ({userList}) => {
+
     const {data:session}=useSession();
     const dispatch=useDispatch();
     const router = useRouter();
-    const cart = useSelector(state=>state.cart);
+    let cart = useSelector(state=>state.cart);
     const user = userList?.find((user)=>user.email === session?.user?.email);
-    console.log("ProductCart",cart.products);
+    const [cartProduct,setCartProduct]=useState([]);
 
+
+
+
+       const cartProducts=()=> cart.products.forEach((item)=>{
+            const products={
+                image:item.image,
+                title:item.title,
+                extras:item.extras,
+                price:item.price,
+                productQuantity:item.productQuantity
+            };
+            setCartProduct((prev)=>[...prev,products]);
+        });
+
+
+    useEffect(()=>{
+        cartProducts();
+        console.log("cartProduct",cartProduct);
+    },[cart.products.length])
 
     const newOrder= {
         email:user?.email,
@@ -25,6 +44,8 @@ const Cart = ({userList}) => {
         quantity:cart.totalQuantity,
         total:cart.total,
         method:0,
+        productOrder:cartProduct,
+
     }
 
     const createOrder = async ()=>{
@@ -34,6 +55,7 @@ const Cart = ({userList}) => {
                     const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/orders`,newOrder);
                     if(res.status === 201){
                         dispatch(cartActions.reset())
+                        toast.success("Order created successfully",{autoClose:1000});
                         toast.success("Order created successfully",{autoClose:1000});
                         await router.push(`/order/${res.data._id}`);
 
@@ -49,7 +71,24 @@ const Cart = ({userList}) => {
         }
     }
 
+    const increaseItemHandler = (item)=>{
+        dispatch(cartActions.increaseProduct(item));
 
+    }
+    const decreaseItemHandler=(item)=>{
+        dispatch(cartActions.decreaseProduct(item));
+
+    }
+    const removeItemHandler=(item)=>{
+        dispatch(cartActions.removeProduct(item));
+
+    }
+
+    const productPageHandler=(pathName)=>{
+        router.push("/product/"+pathName._id.toString());
+
+    }
+    console.log(cart.products.map(item=>console.log(item)));
 
     return <div  className="min-h-[calc(100vh_-_433px)]">
         <div className="flex justify-between items-center md:flex-row flex-col">
@@ -66,9 +105,11 @@ const Cart = ({userList}) => {
                       </thead>
                       <tbody className=" ">
                       {cart.products.map((product,index)=>
-                          <tr className=" border-b border-gray-700 bg-secondary
-                    hover:bg-primary hover:border-primary transition-all " key={index}>
-                              <td className="py-2 px-6 font-medium whitespace-nowrap hover:text-white flex items-center justify-center gap-x-2">
+                          <tr className=" border-b border-gray-700 bg-secondary cursor-pointer
+                    hover:bg-primary hover:border-primary transition-all " key={index} >
+                              <td className="py-2 px-6 font-medium whitespace-nowrap hover:text-white flex items-center justify-center gap-x-2"
+                                  onClick={()=>{productPageHandler(product)}}
+                              >
                                   <Image
                                       src={product.image}
                                       alt={product.image}
@@ -78,11 +119,24 @@ const Cart = ({userList}) => {
                                       className="w-auto h-auto"
                                   /> <span>{product.title}</span></td>
                               <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                                  {product.extras.map(item=><span key={item._id}>{item.text}</span>)}</td>
+                                  {
+                                     product.extras?.length >0 ?  (product.extras.map(item=><span key={item._id}>{item.text}</span>)) : ("Empty")
+                                  }</td>
                               <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">${product.price}</td>
-                              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">1</td>
-                          </tr>
+                              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
 
+                               <div className="flex items-center justify-center gap-2 flex-row">
+                                   <button className="border w-4 h-4 rounded-full text-md flex items-center justify-center"
+                                    onClick={()=>increaseItemHandler(product)}
+                                   >+</button>
+                                   <p>{product.productTotal}</p>
+                                   <button className="border w-4 h-4 rounded-full text-md flex items-center justify-center"
+                                   onClick={()=>decreaseItemHandler(product)}
+                                   >-</button>
+                                   <div className="ml-4 cursor-pointer" >  <i className="fa-solid fa-trash" onClick={()=>removeItemHandler(product)}></i></div>
+                               </div>
+                              </td>
+                          </tr>
                       )}
                       </tbody>
                   </table>
