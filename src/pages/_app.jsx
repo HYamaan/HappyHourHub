ef7c28c14f081595ccc6d90de373660f687a0a9c
hakanyaman <hakanyaman5249@gmail.com>
2023-03-21 21:00:52 +0300

    Fetch işlemi yaparken kullanıcıya gösterilmesi için kütüphane ve css dosyaları entegre edildi.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,16 +1,22 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import '../styles/globals.css'
-import Layout from "../RootLayout/Layout";
-
 import React from "react";
+import Layout from "../RootLayout/Layout";
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 import {SessionProvider} from "next-auth/react"
 import {Provider} from "react-redux";
 import store from "../redux/store"
 
+import Router from 'next/router'
+import NProgress from "nprogress";
+
+import "nprogress/nprogress.css";
+import 'react-toastify/dist/ReactToastify.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../styles/globals.css'
+Router.events.on("routeChangeStart",()=>NProgress.start());
+Router.events.on("routeChangeComplete",()=>NProgress.done());
+Router.events.on("routeChangeError",()=>NProgress.error());
 
 
 export default function App({Component, pageProps: {session, ...pageProps}}) {
