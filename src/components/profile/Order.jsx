456b8ae302ddf0f292ce03ddc8629732f26ee415
hakanyaman <hakanyaman5249@gmail.com>
2023-03-23 05:52:35 +0300

    Müşteri kendi sepetine gittiğinde hangi ürünü almışım ve bu ürünlerin durumları neler kısmı eklendi. Daha önce sadece toplam fiyat ve toplam ürün sayısına daha fazla ürün eklemesi yapıldı

--- src/components/profile/Order.jsx
+++ src/components/profile/Order.jsx
@@ -1,15 +1,23 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {useSelector} from "react-redux";
 import Title from "../UI/Title";
 import axios from "axios";
 import {useSession} from "next-auth/react";
+import Image from "next/image";
+import {toast} from "react-toastify";
+import {useRouter} from "next/router";
 
 
 const Order = () => {
     const{data:session}=useSession();
+    const router = useRouter();
     const status = ["Preparing","On the way","Delivered"];
     const [orders,setOrders]=useState([]);
     const [currentUser,setCurrentUser]=useState([]);
+    const[showProducts,setShowProducts]=useState(false);
+
+    const [orderProductsID,setOrderProductsID]=useState("");
+    const [orderProducts,setOrderProducts]=useState([]);
 
     useEffect(()=>{
         const getOrders= async ()=>{
@@ -23,7 +31,7 @@ const Order = () => {
         }
         getOrders();
     },[currentUser]);
-    console.log("ORDERS",orders);
+    //console.log("ORDERS",orders);
 
     useEffect( ()=>{
         const getUser =async ()=>{
@@ -35,11 +43,37 @@ const Order = () => {
             }
         }
         getUser();
-    },[session])
+    },[session]);
 
 
-    return(
-        <form className=" flex-1 lg:p-8 lg:mt-0 mt-5">
+    const handleShowOrdersProducts=(userId)=>{
+        setOrderProductsID(userId);
+
+    } ;  console.log("sdsdsds",orderProductsID);
+    const getHandleProduct=useCallback(async ()=> {
+        try {
+            console.log("orderProductsID", orderProductsID);
+            const products = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/orders/${orderProductsID}`);
+            setOrderProducts(products.data);
+        } catch (err) {
+            console.log(err);
+        }
+    },[setOrderProducts,orderProductsID]);
+
+    useEffect(()=>{
+        console.log("orderProductsID",orderProductsID)
+        getHandleProduct();
+    },[setOrderProductsID,setOrderProducts,orderProductsID]);
+
+
+const showProductMenu=  (productId)=>{
+    router.push(`/product/${productId.toString()}`);
+    console.log("showProductMenu",productId)
+}
+
+    return<>
+        {(!showProducts ? (
+            <form className=" flex-1 lg:p-8 lg:mt-0 mt-5">
             <Title className="text-[40px]">Account Settings</Title>
             <div className="overflow-x-auto w-full">
                 <table className="w-full text-sm text-center text-gray-500 min-w-[1000px] ">
@@ -57,8 +91,9 @@ const Order = () => {
                         orders.length>0 &&
                         orders.map((order)=>(
                             <tr className=" border-b border-gray-700 bg-secondary
-                    hover:bg-primary hover:border-primary transition-all" key={order._id} >
-                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center justify-center gap-x-2">
+                    hover:bg-primary hover:border-primary transition-all" key={order._id} onClick={()=>{handleShowOrdersProducts(order._id); setShowProducts(true);}}>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center justify-center gap-x-2"
+                                >
                                     <span>{order._id.substring(0,5)}...</span></td>
                                 <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white "><span>{order?.address}</span></td>
                                 <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">{order?.updatedAt.slice(0,10)}</td>
@@ -73,7 +108,64 @@ const Order = () => {
                     </tbody>
                 </table>
             </div>
-        </form>
-    );
+        </form>)
+            :
+            (
+            <form className=" flex-1 lg:p-8 lg:mt-0 mt-5">
+            <Title className="text-[40px]">Account Settings</Title>
+            <div className="overflow-auto w-full h-[370px]">
+                <table className="w-full text-sm text-center text-gray-500 min-w-[1000px] ">
+                    <thead className="text-xs bg-gray-700 text-gray-400 uppercase">
+                    <tr>
+                        <th scope="col" className="py-3 px-6">PRODUCT</th>
+                        <th scope="col" className="py-3 px-6">EXTRAS</th>
+                        <th scope="col" className="py-3 px-6">PRICE</th>
+                        <th scope="col" className="py-3 px-6">QUANTITY</th>
+                        <th scope="col" className="py-3 px-6">STATUS</th>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    {
+                        orderProducts?.productOrder?.length>0 &&
+                        orderProducts?.productOrder.map((orderProduct)=>(
+                            <tr className=" border-b border-gray-700 bg-secondary
+                    hover:bg-primary hover:border-primary transition-all"
+                                key={Math.random()}
+                                onClick={()=>{showProductMenu(orderProduct.orderId)}}
+                            >
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center justify-center gap-x-2">
+                                    <Image
+                                        src={orderProduct.image}
+                                        alt={orderProduct.image}
+                                        width={30}
+                                        height={30}
+                                        priority={true}
+                                        className="w-auto h-auto"
+                                    /> <span>{orderProduct.title}</span>
+                                </td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                                    <span>
+                                        {
+                                            orderProduct.extras?.length >0
+                                                ?
+                                                (orderProduct.extras.map(item=><span key={item._id}>{item.text}</span>))
+                                                :
+                                                ("Empty")
+                                        }
+                                    </span>
+                                </td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">{orderProduct.price}</td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">{orderProduct?.quantity}</td>
+                                <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">{status[orderProducts.status]}</td>
+                            </tr>
+                        ))
+                    }
+
+
+                    </tbody>
+                </table>
+            </div>
+        </form>))}
+    </>
 }
 export default Order;
