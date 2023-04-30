3eda1e95893d1ee4f4861d5338f68d00f62f5a12
hakanyaman <hakanyaman5249@gmail.com>
2023-04-30 19:20:21 +0300

    nodemailer

---
+++ src/utilities/nodeEmail.js
@@ -0,0 +1,34 @@
+
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options)=>{
+    // create reusable transporter object using the default SMTP transport
+
+    let transporter = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "881abf84fbf4ea",
+            pass: "a536cf30ca742e"
+        }
+    });
+
+    //DEFINE THE EMAIL OPTİONS
+    const mailOptions = {
+        from: '"HAKAN YAMAN 👻" <yaman@inveon.com>', // sender address
+        to: options.email, // list of receivers
+        subject: options.subject, // Subject line
+        text: options.message, // plain text body
+        html: "<b>Hello world?</b>", // html body
+    }
+
+    // send mail with defined transport object
+    let info =await transporter.sendMail(mailOptions);
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+export default sendEmail;
