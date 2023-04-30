e9728497e2809402d6c97513dc88a73055d0f8c1
hakanyaman <hakanyaman5249@gmail.com>
2023-04-30 20:13:19 +0300

    fixed the line on the submitting category page of the continuous request.

--- src/components/admin/Category.jsx
+++ src/components/admin/Category.jsx
@@ -2,44 +2,53 @@ import React, {useEffect, useState} from "react";
 import Title from "../UI/Title";
 import Input from "../form/Input";
 import axios from "axios";
-import {set} from "mongoose";
 
 const Category = () => {
     const [inputText, setInputText] = useState("");
     const [categories, setCategories] = useState([]);
+    const [createCategory,setCreateCategory]=useState(false);
 
-    useEffect(() => {
-        const getCategories = async () => {
-            try {
-                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
-                setCategories(res?.data);
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        getCategories();
-    }, [inputText,categories]);
+
     const handleCreate = async () => {
         try {
-            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/categories`,{title:inputText});
-            setCategories([...categories,res]);
-            setInputText("");
+            const inputTitle = inputText.charAt(0).toUpperCase() + inputText.slice(1);
+            const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/categories`, {title: inputTitle});
+            if (res.status === 200) {
+
+                setCategories([...categories, res]);
+                setInputText("");
+                setCreateCategory(true)
+            }
 
         } catch (err) {
             console.log("CategoryAdmin", err);
         }
     }
-    const handleDelete=async (id)=>{
+    const handleDelete = async (id) => {
         try {
-            if(confirm("Are you sure you want to delete this category?")){
-                const res = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`);
-                setCategories(categories.filter(el=>el._id !== id));
+            if (confirm("Are you sure you want to delete this category?")) {
+                 await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`);
+                setCategories(categories.filter(el => el._id !== id));
             }
 
-        }catch (err){
+        } catch (err) {
             console.log(err);
         }
     }
+    useEffect(() => {
+        const getCategories = async () => {
+            try {
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/categories`);
+                if (res.status === 200) {
+                    setCategories(res.data);
+                    setCreateCategory(false)
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        getCategories();
+    }, [createCategory]);
 
 
     return (<div className=" flex-1 lg:p-8 lg:mt-0 mt-5">
@@ -57,12 +66,13 @@ const Category = () => {
                 </button>
             </div>
             <div className="mt-10 max-h-[210px] overflow-auto">
-                {categories.map((category) =>
-                    <div className="flex justify-between mt-4" key={category._id}>
+                {categories.map((category,index) =>
+                    <div key={category._id || index} className="flex justify-between mt-4"
+                         >
                         <b className="text-xl">{category.title}</b>
                         <button className="btn-primary !bg-danger"
-                                onClick={() =>handleDelete( category._id)}>Delete
-                        </button>
+                                onClick={() => handleDelete(category._id)}>Delete
+                         </button>
                     </div>
                 )}
 
