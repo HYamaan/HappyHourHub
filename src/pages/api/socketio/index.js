4531b692319caf1752d4769fe03bfe8c296f104b
hakanyaman <hakanyaman5249@gmail.com>
2023-05-03 23:55:17 +0300

    socket.io first commit

---
+++ src/pages/api/socketio/index.js
@@ -0,0 +1,56 @@
+import { Server } from 'socket.io'
+
+const SocketHandler = (req, res) => {
+    let users=[];
+    const addUser = (userId,socketId)=>{
+        !users.some(user=>user.userId===userId) && users.push({userId,socketId});
+    }
+    const removeUser = (socketId)=>{
+        users = users.filter(user => user.socketId !== socketId);
+    }
+    const getUser = (userId)=>{
+        return users.find(user => user.userId === userId)
+    }
+    if (res.socket.server.io) {
+        console.log('Socket is already running')
+    } else {
+        console.log('Socket is initializing')
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+
+        io.on('connection', socket => {
+            console.log("a user connected")
+
+            //take userId and socketId from user
+            socket.on("addUser",(userId)=>{
+                console.log("users",userId)
+                addUser(userId,socket.id);
+                io.emit("getUsers",users);
+            });
+
+            //send and get message
+            socket.on("sendMessage",({senderId,reciverId,text})=>{
+                const user= getUser(reciverId);
+                io.to(user.socket).emit("getMessage",{
+                    senderId,text
+                })
+            })
+
+            //when disconnect
+            socket.on("disconnect",()=>{
+                removeUser(socket.id)
+                console.log("a user disconnected",socket.id);
+                io.emit("getUsers",users);
+            });
+
+            socket.on('input-change', msg => {
+                console.log("msf11",msg)
+                socket.emit('update-input', msg)
+            });
+
+        })
+    }
+    res.end()
+}
+
+export default SocketHandler
---
+++ src/pages/socketio/index.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { useSession } from "next-auth/react";
+
+const SocketIo = () => {
+    const [socket, setSocket] = useState();
+    const [input, setInput] = useState("");
+    const [arrivalMessage,setArrivalMessage]=useState(null);
+    const [messages,setMessages]=useState([]);
+    const { data: session } = useSession();
+
+    useEffect(() => {
+        const socketInitializer = async () => {
+            await fetch(`${process.env.NEXT_PUBLIC_API_URL}/socketio`);
+            const newSocket = io("ws://localhost:3000");
+            setSocket(newSocket);
+
+            newSocket.on("connect", (data) => {
+                console.log(data);
+                console.log("connected");
+            });
+
+            newSocket.on("update-input", (msg) => {
+                setInput(msg);
+            });
+
+            newSocket.on("getMessage",data=>{
+                setArrivalMessage({
+                    sender:data.senderId,
+                    text:data.text,
+                    createdAt:Date.now()
+                });
+            })
+        };
+        socketInitializer();
+
+
+    }, []);
+    useEffect(()=>{
+        arrivalMessage && setMessages(prev =>[...prev,arrivalMessage]);
+    },[arrivalMessage])
+
+    useEffect(() => {
+        if (socket && session?.user?.id) {
+            socket.emit("addUser", session?.user?.id);
+            socket.on("getUsers",users=>{
+                console.log("users",users)
+            })
+        }
+    }, [session, socket]);
+
+    const onChangeHandler = (e) => {
+        setInput(e.target.value);
+        socket.emit("input-change", e.target.value);
+        socket.emit("sendMessage",{
+            senderId:session?.user.id,
+            reciverId:"null",
+            text:"Hello"
+        })
+    };
+    console.log(messages)
+
+    return <>
+        <input
+            placeholder="Type something"
+            value={input}
+            onChange={onChangeHandler}
+        />
+
+    </>
+};
+
+export default SocketIo;
