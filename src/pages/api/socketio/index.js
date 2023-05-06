332725fbb28055d8cca1efe242f70eca0a3c0a46
hakanyaman <hakanyaman5249@gmail.com>
2023-05-06 18:42:50 +0300

    socket.io link is set up for admin page and user content.

--- src/pages/api/socketio/index.js
+++ src/pages/api/socketio/index.js
@@ -1,13 +1,14 @@
-import { Server } from 'socket.io'
+import {Server} from 'socket.io'
+
 const SocketHandler = (req, res) => {
-    let users=[];
-    const addUser = (userId,socketId)=>{
-        !users.some(user=>user.userId===userId) && users.push({userId,socketId});
+    let users = [];
+    const addUser = (userId, socketId) => {
+        !users.some(user => user.userId === userId) && users.push({userId, socketId});
     }
-    const removeUser = (socketId)=>{
+    const removeUser = (socketId) => {
         users = users.filter(user => user.socketId !== socketId);
     }
-    const getUser = (userId)=>{
+    const getUser = (userId) => {
         return users.find(user => user.userId === userId)
     }
     if (res.socket.server.io) {
@@ -19,33 +20,47 @@ const SocketHandler = (req, res) => {
 
         io.on('connection', socket => {
             console.log("a user connected")
-
             //take userId and socketId from user
-            socket.on("addUser",(userId)=>{
-                console.log("users",userId)
-                addUser(userId,socket.id);
-                io.emit("getUsers",users);
-            });
+            socket.on("addUser", userId => {
 
+                addUser(userId, socket.id);
+                io.emit("getUsers", users)
+            });
             //send and get message
-            // socket.on("sendMessage",({senderId,reciverId,text})=>{
-            //     const user= getUser(senderId); //reciverId
-            //     io.to(user?.socket).emit("getMessage",{
-            //         senderId,text
-            //     })
-            // })
-
-            //when disconnect
-            socket.on("disconnect",()=>{
-                removeUser(socket.id)
-                console.log("a user disconnected",socket.id);
-                io.emit("getUsers",users);
+            socket.on("sendMessage", ({senderId, receiverId, text}) => {
+                const user = getUser(receiverId);
+                if (user && user.socketId) {
+                    io.to(user.socketId).emit("getMessage", {
+                        senderId, text
+                    });
+                } else {
+                    console.log(`Could not find user with id ${receiverId} or user does not have a socketId`);
+                }
             });
 
 
-            socket.on('input-change', msg => {
-                socket.broadcast.emit('update-input', msg)
+
+            //When disconnect
+            socket.on("disconnect", () => {
+                console.log("a user disconnected!");
+                removeUser(socket.id);
+                io.emit("getUsers", users)
             })
+            //When disconnect with CustomerService
+            socket.on("disconnectUser", (userId) => {
+                console.log(`Disconnecting user with id ${userId}`);
+                const disconnectedSocket = users.find(s => s.userId === userId);
+                if (disconnectedSocket) {
+                    removeUser(disconnectedSocket.socketId);
+                    io.to(`${userId}`).disconnectSockets(true);
+                    io.emit("getUsers", users);
+                    console.log(`User with id ${userId} disconnected`);
+                } else {
+                    console.log(`Could not find user with id ${userId}`);
+                }
+            });
+
+
         })
     }
     res.end()
