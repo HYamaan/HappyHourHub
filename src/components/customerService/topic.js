c46cfcc22f4eae4b871f4da3423ff4f34b75375d
hakanyaman <hakanyaman5249@gmail.com>
2023-05-06 18:44:04 +0300

    Added a search bar if there is more than one topic in the topic section

--- src/components/customerService/topic.js
+++ src/components/customerService/topic.js
@@ -1,6 +1,6 @@
+import {useState} from 'react';
 
-import {useState, useRef} from 'react';
-const Topic = ({setTopicId,setCurrentChat})=>{
+const Topic = ({setTopicId, setCurrentChat}) => {
     const [options, setOptions] = useState([
         {"1": "Teknik Destek"},
         {"2": "Fatura Sorunu"},
@@ -8,44 +8,54 @@ const Topic = ({setTopicId,setCurrentChat})=>{
         {"4": "Diğer"}
     ]);
     const [text, setText] = useState('');
-    const inputRef = useRef(null);
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-            const enteredText = event.target.value;
-            // Burada veriyi işleyebilirsiniz
-            setText(enteredText);
-            inputRef.current.value = '';
-        }
-    };
+    const [selectTopic, setSelectTopic] = useState("");
 
 
-    return  <div className="max-h-[calc(100vh-6.8rem)] overflow-y-auto mt-4 pr-1">
+    const filteredOptions = options.filter((option) => {
+        const optionValue = Object.values(option)[0];
+        return optionValue.toLowerCase().includes(text.toLowerCase());
+    });
+    const handleTopicClick = (key) => {
+        setCurrentChat(null)
+        setSelectTopic(key)
+    }
 
-            <div className="flex items-center m-2">
-                <input
-                    className="  border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                    type="text" placeholder="Type your message..."
-                    ref={inputRef} onKeyDown={handleKeyDown}
-                />
-                <i className="fas fa-paper-plane"></i>
-            </div>
+    return <div className="max-h-[calc(100vh-6.8rem)] overflow-y-auto mt-4 pr-1">
 
-            <div className="flex flex-col items-center justify-content  ">
 
-                {options.map((option) => {
-                    const optionKey = Object.keys(option)[0];
-                    const optionValue = option[optionKey];
-
-                    return <div key={optionKey} className="w-full text-lg bg-white hover:bg-tertiary mt-1 py-3 cursor-pointer"
-                    onClick={()=>setCurrentChat(null)}>
-                        <div className="flex ml-10" onClick={()=>{setTopicId(optionKey)}}>
-                            <span className="mr-3 font-bold">#</span>
-                            <span>{optionValue}</span>
-                        </div>
-                    </div>;
-                })}
-            </div>
+        <div className="relative text-gray-600 focus-within:text-gray-400 mb-2">
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                       viewBox="0 0 24 24" className="w-6 h-6"><path
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </button>
+              </span>
+            <input type="search" name="search"
+                   className="py-2 w-full text-sm text-white bg-gray-900 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-900"
+                   placeholder="Search..." autoComplete="off" onChange={(e)=>setText(e.target.value)}/>
         </div>
+        <div className="flex flex-col items-center justify-content  ">
+
+            {filteredOptions.map((option) => {
+                const optionKey = Object.keys(option)[0];
+                const optionValue = option[optionKey];
+
+                return <div key={optionKey}
+                            className={`w-full text-lg  mt-1 py-3 cursor-pointer ${selectTopic === optionKey ? "bg-stateGray hover:bg-cadetGray font-bold" : "bg-white hover:bg-tertiary"}`}
+                            onClick={() => {
+                                handleTopicClick(optionKey)
+                            }}>
+                    <div className="flex ml-10" onClick={() => {
+                        setTopicId(optionKey)
+                    }}>
+                        <span className="mr-3 font-bold">#</span>
+                        <span>{optionValue}</span>
+                    </div>
+                </div>;
+            })}
+        </div>
+    </div>
 
 
 }
