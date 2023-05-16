372945706f182445a3b3060550b224a700d157f0
hakanyaman <hakanyaman5249@gmail.com>
2023-05-16 17:43:55 +0300

    profileSchema update

--- src/schema/profileSchema.js
+++ src/schema/profileSchema.js
@@ -7,9 +7,6 @@ export const profileSchema = Yup.object({
     phoneNumber: Yup.string()
         .required('Phone required'),
     email: Yup.string().email('Invalid email address').required('Email is required'),
-    address: Yup.string()
-        .min(10, 'Address must be at least 10 characters ')
-        .required('Address is required'),
     zipCode: Yup.string()
         .min(4, 'zip Code must be at least 4 characters ')
         .required('zipCode is required'),
