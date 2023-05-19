79a0b21dd95cc823271bc69054000248cc39e4ef
hakanyaman <hakanyaman5249@gmail.com>
2023-05-19 17:08:41 +0300

    verify code useFromik Schema updated

--- src/schema/verifyCode.js
+++ src/schema/verifyCode.js
@@ -4,8 +4,6 @@ export const verifyCodeSchema = Yup.object({
 
 
     verifyCode: Yup.string()
-        .max(6, 'Must be 6 characters ')
-        .min(6, 'Must be 6 characters ')
         .required('Verify Code required'),
 
 
