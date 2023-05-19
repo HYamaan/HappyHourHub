b37c39bcae8263248c4f3fb612b5f05183301dd1
hakanyaman <hakanyaman5249@gmail.com>
2023-05-19 17:15:15 +0300

    sepette ürün yoksa göstermeme eklendi

--- src/components/layout/shoppingBasket.js
+++ src/components/layout/shoppingBasket.js
@@ -83,16 +83,17 @@ const ShoppingBasket = ({router,isMenuModal,showBasket,setShowBasket})=>{
                  onMouseLeave={() => {
                 setShowBasket(false);
             }}>
-                <div  className="overflow-x-hidden overflow-y-auto max-h-[20.813rem] pt-4 px-2">
-                    {cart.products.map((prod,index) => {
-                        const isLastItem = index === cart.products.length - 1;
-                        return <div key={index}
-                                    className={`flex items-center flex-row w-[21.688rem] h-[6rem] hover:bg-[#D3D6D9] 
+                {cart.products.length !==0 ? (<>
+                    <div  className="overflow-x-hidden overflow-y-auto max-h-[20.813rem] pt-4 px-2">
+                        {cart.products.map((prod,index) => {
+                            const isLastItem = index === cart.products.length - 1;
+                            return <div key={index}
+                                        className={`flex items-center flex-row w-[21.688rem] h-[6rem] hover:bg-[#D3D6D9] 
                            ${!isLastItem ? "border-b-2 border-secondary border-opacity-40 " :""}`}
 
-                        >
-                            <div className="basis-3/12 self-center cursor-pointer w-full" >
-                                <Link href={`/product/${prod._id}`}>
+                            >
+                                <div className="basis-3/12 self-center cursor-pointer w-full" >
+                                    <Link href={`/product/${prod._id}`}>
                                         <Image
                                             src={prod?.image}
                                             alt={prod?.image}
@@ -104,29 +105,30 @@ const ShoppingBasket = ({router,isMenuModal,showBasket,setShowBasket})=>{
                                             onClick={() =>  setShowBasket(false)}
                                         />
 
-                                </Link>
+                                    </Link>
+                                </div>
+                                <div className="basis-8/12 self-start text-xs text-cadetGray ml-2 mt-2" >
+                                    <p >{prod.title}</p>
+                                    <p>{`${prod.extras?.length>0 ? "Options: " : ""}${prod?.extras?.map(ext=>ext.text).join(', ')}`}</p>
+                                    <p>{new Intl.NumberFormat('tr-TR', {style: 'currency', currency: 'TRY', minimumFractionDigits: 2}).format((prod.price))}₺</p>
+                                    <p>{`Adet: ${prod.productTotal}`}</p>
+                                </div>
+                                <div className="basis-1/12 h-full mt-2 place-self-start text-secondary hover:text-danger text-xl cursor-pointer "
+                                     onClick={()=> {
+                                         setProduct(prod)
+                                         deleteProduct(prod)
+                                     }}
+                                >
+                                    <i className="fa-solid fa-trash-can"></i></div>
                             </div>
-                            <div className="basis-8/12 self-start text-xs text-cadetGray ml-2 mt-2" >
-                                <p >{prod.title}</p>
-                                <p>{`${prod.extras?.length>0 ? "Options: " : ""}${prod?.extras?.map(ext=>ext.text).join(', ')}`}</p>
-                                <p>{new Intl.NumberFormat('tr-TR', {style: 'currency', currency: 'TRY', minimumFractionDigits: 2}).format((prod.price))}₺</p>
-                                <p>{`Adet: ${prod.productTotal}`}</p>
-                            </div>
-                            <div className="basis-1/12 h-full mt-2 place-self-start text-secondary hover:text-danger text-xl cursor-pointer "
-                                 onClick={()=> {
-                                     setProduct(prod)
-                                     deleteProduct(prod)
-                                 }}
-                            >
-                                <i className="fa-solid fa-trash-can"></i></div>
-                        </div>
-                    })}
-                </div>
-                <div className="flex items-center justify-between h-14 w-full bg-[#D3D6D9] text-secondary">
-                    <span className="ml-12">Ara Toplam</span>
-                    <span className="font-bold mr-4 ">{new Intl.NumberFormat('tr-TR', {style: 'currency', currency: 'TRY', minimumFractionDigits: 2}).format((cart.total))}₺</span>
-                    <div className="bg-primary rounded-2xl py-2 px-4 mr-2 text-tertiary hover:bg-primaryBold cursor-pointer" onClick={()=>moveToBasket()}>Sepete Git</div>
-                </div>
+                        })}
+                    </div>
+                    <div className="flex items-center justify-between h-14 w-full bg-[#D3D6D9] text-secondary">
+                        <span className="ml-12">Ara Toplam</span>
+                        <span className="font-bold mr-4 ">{new Intl.NumberFormat('tr-TR', {style: 'currency', currency: 'TRY', minimumFractionDigits: 2}).format((cart.total))}₺</span>
+                        <div className="bg-primary rounded-2xl py-2 px-4 mr-2 text-tertiary hover:bg-primaryBold cursor-pointer" onClick={()=>moveToBasket()}>Sepete Git</div>
+                    </div>
+                </>):(<div className="flex items-center justify-center py-4 px-6  leading-6 text-left tracking-wider text-payneGray  lg:text-base text-sm">Sepetinizde ürün bulunmamaktadır</div>)}
             </div>
   }
     </>
