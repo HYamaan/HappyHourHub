1b0d817d829656b406ad0f577046b47eb7ea3004
hakanyaman <hakanyaman5249@gmail.com>
2023-05-19 17:04:45 +0300

    Order model

--- src/models/Order.js
+++ src/models/Order.js
@@ -2,16 +2,32 @@ import mongoose from "mongoose";
 
 const OrderSchema = new mongoose.Schema(
     {
+        conversationId:{
+            type:String,
+            required:true
+        },
+        basketId:{
+            type:String,
+            required:true
+        },
+        name:{
+            type:String,
+            required:true,
+        },
+        surname:{
+            type:String,
+            required:true,
+        },
         email: {
             type: String,
             required: true,
         },
-        customer:{
-            type:String,
+
+        price:{
+            type:Number,
             required:true,
-            maxlength:100,
         },
-        total:{
+        paidPrice:{
             type:Number,
             required:true,
         },
@@ -19,21 +35,54 @@ const OrderSchema = new mongoose.Schema(
             type:Number,
             required:true,
         },
-        address:{
-            type:String,
-            required:true,
-            maxlength:200,
+        installment:{
+            type:Number,
         },
+        address:[
+            {
+                contactName:{
+                    type:String
+                },
+                country: {
+                    type: String,
+                },
+                city:{
+                    type: String,
+                },
+                address1:{
+                    type: String,
+                },
+            }
+        ],
+
         status:{
             type:Number,
-            default:0,
+            default:-1,
         },
-        method:{
-            type:Number,
+        completed:{
+            type:Boolean,
+            default:false,
+            required:true
         },
+
         productOrder:{
             type:[],
         },
+
+        currency:{
+            type:String,
+            required:true,
+            default: "TRY",
+            enum:["TRY","USD","EUR"]
+        },
+        cargo:{
+            type:String,
+            default:"Yurt içi Kargo",
+            enum: ["Yur içi Kargo","Aras Kargo", "GKN Kargo"],
+        },
+        paymentSuccessId:{
+            type:String
+        }
 
     },
     {timestamps:true}
